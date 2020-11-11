86d5000ecc277864a6b6865c64a6a9b0f6bc64ff
Azathoth <m.sluijtman@sintlucasedu.nl>
2020-11-11 14:33:31 +0100

    Added shooting bsics

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -1,20 +1,26 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MouseLook : MonoBehaviour{
     private float mouseX;
     private float mouseY;
-
     private float xRotation;
-    
     public float mouseSensitivity = 100;
     public Transform playerBody;
+
+    private float rayDistance = 10f;
+    private Ray ray;
+    private RaycastHit hit;
+
+    public Transform gunBarrel;
 
     private void Start(){
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update(){
+        if (Input.GetButtonDown("Fire1"))
+            Shoot();
+
         mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
@@ -23,5 +29,15 @@ public class MouseLook : MonoBehaviour{
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private void Shoot(){
+        ray = new Ray(gunBarrel.position, transform.forward);
+
+        Debug.DrawRay(gunBarrel.position, transform.forward, Color.red);
+
+        if (Physics.Raycast(ray, out hit, rayDistance)){
+            print("Shit hit");
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour{
+    // Declaration of variables
     private float x;
     private float z;
     private CharacterController controller;
     private Vector3 move;
     private float speed = 10f;
     private Vector3 velocity;
-    private float gravity = -9.8f;
+    private float gravity = -20f;
+    private float jumpHeight = 3f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
 
     private void Start(){
         controller = GetComponent<CharacterController>();
     }
 
     private void Update(){
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        if (Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        
         x = Input.GetAxis("Horizontal");
         z = Input.GetAxis("Vertical");
 
