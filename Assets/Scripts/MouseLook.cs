d4e5033357fc4bbbd86786b2fbc17a8a76d1d947
Azathoth <m.sluijtman@sintlucasedu.nl>
2020-11-11 12:53:28 +0100

    Added first person controller basis (all hail Brackeys)

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour{
+    private float mouseX;
+    private float mouseY;
+
+    private float xRotation;
+    
+    public float mouseSensitivity = 100;
+    public Transform playerBody;
+
+    private void Start(){
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update(){
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+    private float x;
+    private float z;
+    private CharacterController controller;
+    private Vector3 move;
+    private float speed = 10f;
+    private Vector3 velocity;
+    private float gravity = -9.8f;
+
+    private void Start(){
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update(){
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
